26b3309fd32cc83628060693129af8ad981aa046
zotel1 <zotelsigel@gmail.com>
2024-07-11 14:57:40 -0300

    Creamos ultimo challenge alura latam

---
+++ src/main/java/com/alura/challenge/challengeForohub/ChallengeForohubApplication.java
@@ -0,0 +1,13 @@
+package com.alura.challenge.challengeForohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeForohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeForohubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/challenge/challengeForohub/ChallengeForohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.challenge.challengeForohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeForohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
