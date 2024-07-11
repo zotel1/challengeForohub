7eca1c0befd7d56f49281cca06af85cb4ba4edd0
zotel1 <zotelsigel@gmail.com>
2024-07-11 15:35:07 -0300

    update, agregamos el constructor de la clase topico, con la informacion de los record en dto

---
+++ src/main/java/com/alura/challenge/challengeForohub/model/Usuario.java
@@ -0,0 +1,24 @@
+package com.alura.challenge.challengeForohub.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+
+}
