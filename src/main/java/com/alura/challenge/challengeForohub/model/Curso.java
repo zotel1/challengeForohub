f1186b530d419f1f22b72252e1a3d7e0017a77c1
zotel1 <zotelsigel@gmail.com>
2024-07-11 15:18:05 -0300

    uodate

---
+++ src/main/java/com/alura/challenge/challengeForohub/model/Curso.java
@@ -0,0 +1,9 @@
+package com.alura.challenge.challengeForohub.model;
+
+public enum Curso {
+    ADMINISTRACION_DE_PROYECTOS,
+    BASES_DE_DATOS,
+    CALCULO,
+    PROGRAMACION,
+    REDES
+}
---
+++ src/main/java/com/alura/challenge/challengeForohub/model/Topico.java
@@ -0,0 +1,34 @@
+package com.alura.challenge.challengeForohub.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Table(name = "topicos")
+@Entity(name = "Topico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Topico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String mensaje;
+
+    @CreationTimestamp
+    private LocalDateTime fechaDeCreacion;
+
+    @Enumerated(EnumType.STRING)
+    private Curso curso;
+
+    public Topico() {}
+
+}
